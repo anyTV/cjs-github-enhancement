0ba973a4d8de2562fed0bbe754702045e419db12
pandafox <allen@freedom.tm>
2016-02-22 14:41:45 +0800

    1. Append approval instead of overwrite already typed comment.

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ function cjs_update() {
 }
 
 function allenApproves() {
-    $('#new_comment_field').val(':AllenApproves:');
+	var cmt = $('#new_comment_field').val();
+    $('#new_comment_field').val(cmt + (cmt ? '\n' : '') + ':AllenApproves:');
     $('#partial-new-comment-form-actions').children().get(0).click()
 }
 
